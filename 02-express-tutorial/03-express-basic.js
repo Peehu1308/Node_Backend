ffafd263fe427701bddae2741b19b8da27ca943a
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-12 23:29:19 +0530

    Create 03-express-basic.js

---
+++ 02-express-tutorial/03-express-basic.js
@@ -0,0 +1,31 @@
+const express= require('express');
+
+const app=express()
+
+app.get('/',(req,res)=>{
+    console.log("user hit the resource")
+    res.status(200).send('Home page')
+})
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('About page')
+})
+app.all('*',(req,res)=>{
+    res.status(404).send("<h1>Resource not found</h1>")
+})
+
+app.listen(5000,()=>{
+    console.log(`server is listening to port 5000`)
+})
+
+
+// METHODS THAT WE WILL USE
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
+
+
