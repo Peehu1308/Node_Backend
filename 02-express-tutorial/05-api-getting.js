b403402764f7af500cc5b62050c2bd32f26215e1
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-13 22:51:58 +0530

    Create 05-api-getting.js

---
+++ 02-express-tutorial/05-api-getting.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const app=express();
+const {products,people}=require('./data.js');
+
+
+app.get('/',(req,res)=>{
+   res.send('<h1>HomePage</h1><a href="/api/products"a>products</a>')
+})
+app.get('/api/products/:productID',(req,res)=>{
+    // console.log(req);
+    console.log(req.params);
+    const {productID}=req.params;
+    const singleProduct=products.find(
+        (product=>product.id===Number(productID))
+    )
+    if(!singleProduct){
+        return res.status(404).send("product doesnt exist")
+    }
+
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productId/reviews/:reviewId',(req,res)=>{
+    // console.log/(req.params);
+    
+    res.send('hello world')
+})
+
+//  /api/v1/query?search=bh&limit=7678
+
+app.get('/api/v1/query',(req,res)=>{
+    console.log(req.query)
+    const {search,limit}=req.query;
+    let sortedProducts=[...products];
+
+    if(search){
+        sortedProducts=sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts=sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length<1){
+        // res.status(200).send("no products match your serach");
+        return res.status(200).json({success:true,data:[]})
+    }
+    res.status(200).json(sortedProducts);
+    // res.send("hello peehu")
+})
+
+
+app.get('*',(req,res)=>{
+    res.end("<h1>resource doesnt exist</h1>")
+})
+
+app.get('/api/products',(req,res)=>{
+    const newProducts=products.map((product)=>{
+        const {id,name,image}=product;
+        return {id,name,image};
+    })
+    res.json(newProducts)
+})
+
+
+app.listen(5000,()=>[
+    console.log('server is listening')
+])
