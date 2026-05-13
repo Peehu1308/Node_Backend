d7861c3582c9f65e537693319a4c644fcb9ad292
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-14 00:18:51 +0530

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,11 +4,19 @@ const app=express();
 // req=>middleware=>res
 
 app.get('/',(req,res)=>{
-    res.send('Home')
+    const method=req.method;
+    const url=req.url;
+    const time=new Date().getFullYear();
+    console.log(method,url,time);
+    return res.send('Home')
 })
 
-app.get('/about',(res,req)=>{
-    res.send('Home');
+app.get('/about',(req,res)=>{
+    const method=req.method;
+    const url=req.url;
+    const time=new Date().getFullYear();
+    console.log(method,url,time);
+    return res.send('About');
 })
 
 app.listen(5000,()=>[
