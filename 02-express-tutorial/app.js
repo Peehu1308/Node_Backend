84cd166defb43f05a3ec499a89bb9c0eb3e550dc
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-13 21:49:39 +0530

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,15 @@
-const express=require("express")
-const app=express()
+const express=require('express');
+const app=express();
+const {products,people}=require('./data.js');
+
+
+app.get('/',(req,res)=>{
+    return res.json([products])
+})
+app.get('/about',(req,res)=>{
+    return res.json([people])
+})
+
+app.listen(5000,()=>[
+    console.log('server is listening')
+])
