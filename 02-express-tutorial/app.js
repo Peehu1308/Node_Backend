0afe7e933117726343d66840ea635fb4befe38b7
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-13 23:11:32 +0530

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,15 @@
 const express=require('express');
 const app=express();
 
+// req=>middleware=>res
 
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+
+app.get('/about',(res,req)=>{
+    res.send('Home');
+})
 
 app.listen(5000,()=>[
     console.log('server is listening')
