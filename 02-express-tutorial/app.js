33ce451ed0eb76127380fda00fc11b4db0e8d5fa
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-14 21:32:12 +0530

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,37 +1,13 @@
 const express=require('express');
 const app=express();
-const morgan=require('morgan')
-const logger=require('./logger.js')
-const authorize=require('./authorize.js')
-// req=>middleware=>res
+let {people}=require('./data')
 
-app.use(morgan('tiny'))
-// app.use([logger,authorize])
+// static assets
+app.use(express.static('./methods-public'))
 
-// app.use('/api',logger);
-// api/home/about/products
 
-// app.get('/',logger,(req,res)=>{
-    
-//     res.send('Home')
-// })
-app.get('/',(req,res)=>{
-    
-    res.send('Home')
-    console.log(req.user)
-})
-
-app.get('/about',(req,res)=>{
-    
-    return res.send('About');
-})
-app.get('/api/products',(req,res)=>{
-    
-    return res.send('products');
-})
-app.get('/api/items',(req,res)=>{
-    
-    return res.send('items');
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
 })
 
 app.listen(5000,()=>[
