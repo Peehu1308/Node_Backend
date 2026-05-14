ffb7928893f38e6cc5b12edee1407926ef04792f
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-15 00:02:05 +0530

    Create 06-morgan.js

---
+++ 02-express-tutorial/06-morgan.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const app=express();
+const morgan=require('morgan')
+const logger=require('./logger.js')
+const authorize=require('./authorize.js')
+// req=>middleware=>res
+
+app.use(morgan('tiny'))
+// app.use([logger,authorize])
+
+// app.use('/api',logger);
+// api/home/about/products
+
+// app.get('/',logger,(req,res)=>{
+    
+//     res.send('Home')
+// })
+app.get('/',(req,res)=>{
+    
+    res.send('Home')
+    console.log(req.user)
+})
+
+app.get('/about',(req,res)=>{
+    
+    return res.send('About');
+})
+app.get('/api/products',(req,res)=>{
+    
+    return res.send('products');
+})
+app.get('/api/items',(req,res)=>{
+    
+    return res.send('items');
+})
+
+app.listen(5000,()=>[
+    console.log('server is listening')
+])
