e68d06fce721356faa301d569f638d398993f6ea
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-16 20:25:56 +0530

    Update people.js

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,52 +1,23 @@
 const express=require("express")
 const router=express.Router();
 
-let {people}=require('../data')
+const {
+    getPeople,
+    createPerson,
+    createPerson,
+    updatePerson,
+    createPersonPostman
+}=require('../controllers/people')
 
 router.get('/',)
 
-router.post('/',(req,res)=>{
-    const {name}=req.body
-    if(!name){
-        return res.status(400).json({success:false,msg:'please provide name value'})
-    }
-    res.status(201).json({success:true,person:name})
-})
+router.post('/',)
 
-router.post('/postman',(req,res)=>{
-    const {name}=req.body
-    if(!name){
-        return res
-        .status(400)
-        json({success:false,msg:"please provde the name value"})
-    }
-    res.status(201).json({success:true,data:[...people]})
-})
+router.post('/postman',)
 
 
 
-router.put('/:id',(req,res)=>{
-    const {id}=req.params
-    const {name}=req.body 
-    console.log(id,name)
-    res.send("hello world")
-
-    const person=people.find((person)=>person.id===Number(id))
-
-    if(!person){
-        return res
-        .status(404)
-        json({success:false,msg:`no person with id ${id}`})
-    }
-
-    const newPeople=people.map((person)=>{
-        if(person.id===Number(id)){
-            person.name=name
-        }
-        return person
-    })
-    res.status(200).json({success:true,data:newPeople})
-})
+router.put('/:id',)
 
 router.delete('/:id',(req,res)=>{
     const person=people.find((person)=>person.id===Number(req.params.id))
