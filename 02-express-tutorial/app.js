b19b4293e36bfd628433f281d9da108fd7ffb134
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-16 19:02:49 +0530

    Update app.js

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -70,6 +70,9 @@ app.delete('/api/people/:id',(req,res)=>{
         .status(404)
         .json({success:false,msg:`no person with id ${req.params.id}`})
     }
+
+    const newPeople=people.filter((person)=>person.id!==Number(req.params.id))
+    return res.status(200).json({success:true,data:newPeople})
 })
 
 
