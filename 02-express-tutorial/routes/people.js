e5ea768bcf06c9d169435eca811cb5950fab4419
Peehu Mishra <peehumishra1308@gmail.com>
2026-05-16 20:28:18 +0530

    Update people.js

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -3,34 +3,23 @@ const router=express.Router();
 
 const {
     getPeople,
-    createPerson,
+    deletePerson,
     createPerson,
     updatePerson,
     createPersonPostman
 }=require('../controllers/people')
 
-router.get('/',)
+router.get('/',getPeople)
 
-router.post('/',)
+router.post('/',createPerson)
 
-router.post('/postman',)
+router.post('/postman',createPersonPostman)
 
 
 
-router.put('/:id',)
+router.put('/:id',updatePerson)
 
-router.delete('/:id',(req,res)=>{
-    const person=people.find((person)=>person.id===Number(req.params.id))
-
-    if(!person){
-        return res
-        .status(404)
-        .json({success:false,msg:`no person with id ${req.params.id}`})
-    }
-
-    const newPeople=people.filter((person)=>person.id!==Number(req.params.id))
-    return res.status(200).json({success:true,data:newPeople})
-})
+router.delete('/:id',deletePerson)
 
 
 
